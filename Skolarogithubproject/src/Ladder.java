65da5aed3320a5324088356de772ad67dfbc0914
chandu <chandu@192.168.2.13>
2020-02-05 22:06:25 +0530

    MY FIRST JAVA PROGRAM ABOUT WEEK

---
+++ Skolarogithubproject/src/Ladder.java
@@ -0,0 +1,21 @@
+
+public class Ladder
+{
+	public static void main(String[] args)
+	{
+		int number=0;
+		if(number>0)
+		   {
+			System.out.println("Number is Greater");
+		    }	
+			   else if(number<0)
+			     {
+				System.out.println("Number is Lessar");
+			     }
+			        else if(number==0)
+		         	{
+			         	System.out.println("Number is equal");
+			        }
+		             System.out.println("Ladder");
+	}
+}	

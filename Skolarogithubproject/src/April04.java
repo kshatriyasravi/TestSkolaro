27f22cac1b0dbcfe9ffef0c1abecc3cead027fd8
chandu <chandu@DESKTOP-5C8JFOK>
2020-04-20 11:29:16 +0530

    zss

---
+++ Skolarogithubproject/src/April04.java
@@ -0,0 +1,15 @@
+
+public class April04 
+{
+	public static void main(String[] args)
+	{
+		int[] number= {1,24,23,10,16,15,2,3,5};
+		for(int i=0;i<=24;i++)
+		{
+			System.out.println("Print the first value a[0] is:"+number[1]);
+			System.out.println("Print:"+i);
+			System.out.println("Print\n:"+number[i]);
+		}
+	}
+
+}

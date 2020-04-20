89dcd7f5b07903815e8114d8835ed00835a995ef
chandu <chandu@DESKTOP-5C8JFOK>
2020-04-20 11:28:08 +0530

    fcdd

---
+++ Skolarogithubproject/src/addusingarray.java
@@ -0,0 +1,15 @@
+
+public class addusingarray
+{
+	public static void main(String[] args)
+	{
+		int[] name=new int[3];
+		for(int i=0;i<3;i++)
+		{
+			System.out.println("Print:"+i);
+			System.out.println("\nPrint:"+name[0]);
+		
+		}
+	}
+
+}
